e3899ee2aa6a10715eece4ce3ba35271f35427cf
jishnu2021 <jishnughosh98@gmail.com>
2024-11-08 16:57:59 +0530

    modify app.jsx

--- clientside/src/App.jsx
+++ clientside/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, BrowserRouter, useLocation } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Hero/Home";
 import Blog from "./components/blog/Blog";
@@ -7,16 +7,17 @@ import BlogDetails from "./components/blog/BlogDetails";
 import CoursesPage from "./components/course/CoursesPage";
 import AdminRegister from "./components/registeradmin/AdminRegister";
 import AdminLogin from "./components/registeradmin/AdminLogin";
-import Footer from "./components/footer/Footer";
+// import Footer from "./components/footer/Footer";
 import PaymentPage from "./components/course/PaymentPage";
 import PaymentSuccessPage from "./components/course/PaymentSuccessPage";
 import Feature from "./components/feature/Feature";
 
-function App() {
-  const location = useLocation(); // Access the current route path
+export default function App() {
+  // const location = useLocation(); // Access the current route path
 
   return (
     <>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -29,15 +30,8 @@ function App() {
         <Route path="adminregister" element={<AdminRegister />} />
         <Route path="adminlogin" element={<AdminLogin />} />
       </Routes>
-      {location.pathname === "/" && <Footer />} {/* Render Footer only on Home page */}
+      </BrowserRouter>
+      {/* {location.pathname === "/" && <Footer />} Render Footer only on Home page */}
     </>
-  );
-}
-
-export default function RootApp() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
   );
 }
--- clientside/src/components/navbar/Navbar.jsx
+++ clientside/src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './navbar.css';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   
   return (
@@ -127,45 +127,45 @@ const Navbar = () => {
       </div>
       <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a
-            href="/"
+          <Link
+            to="/"
             className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
             aria-current="page"
           >
             Home
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/feature"
+          <Link
+            to="/feature"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           >
             Feature
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/blog"
+          <Link
+            to="/blog"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           >
             Blog
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/courses"
+          <Link
+            to="/courses"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           >
             Courses
-          </a>
+          </Link>
         </li>
         <li>
-          <a
-            href="/help"
+          <Link
+            to="/help"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           >
             Contact Us
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
